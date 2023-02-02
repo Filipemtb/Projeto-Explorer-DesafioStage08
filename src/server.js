a9df8b9f0b337f8d39aeb2f8644e1cfe470fc1cb
Filipe Mota <99206125+Filipemtb@users.noreply.github.com>
2023-02-02 16:18:43 -0300

    adding routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
-const express = require("express");
+const express = require("express");  //Importando o express do Node_modules
+
+const routes = require("./routes");
+
 
 const app = express();
+app.use(express.json());
+
+app.use(routes);
+
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
